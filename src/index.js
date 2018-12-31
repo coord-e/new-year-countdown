6ab0ce8c3398612490fcf781c7836b87ca79c389
coord.e <me@coord-e.com>
2018-12-31 03:44:54 +0000

    Add: Implement a simple counter

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const { h, render, Component, Color } = require('ink');
 
+const NEWYEAR = new Date(2019, 0, 1, 0, 0, 0, 0);
+
 class Countdown extends Component {
   constructor() {
     super();
@@ -10,10 +12,20 @@ class Countdown extends Component {
   }
 
   render() {
+    const epoch_diff = (NEWYEAR - this.state.time) / 1000;
+    const hour = Math.floor(epoch_diff / 60 / 60);
+    const minute = Math.floor((epoch_diff / 60) % 60);
+    const second = Math.floor(epoch_diff % 60);
+
     return (
-      <Color green>
-        {this.state.time.toString()}
-      </Color>
+      <div>
+        <div>
+          Current date: <Color green>{this.state.time.toString()}</Color>
+        </div>
+        <div>
+          <span>{hour} {minute} <Color red>{second}</Color></span>
+        </div>
+      </div>
     );
   }
 
